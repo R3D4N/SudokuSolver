af1e721db7fdecfe71dd9f3967242710b36a2a70
R3_D4N <bellidoangelo97@gmail.com>
2023-02-13 23:15:54 -0500

    Completed check row and col placement in sodoku-solve.js

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -4,11 +4,27 @@ class SudokuSolver {
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    let index = {
+      'A': [0, 9],
+      'B': [9, 18],
+      'C': [18, 27],
+      'D': [27, 36],
+      'E': [36, 45],
+      'F': [45, 54],
+      'G': [54, 63],
+      'H': [63, 72],
+      'I': [72, 81]
+    }
+    let myRow = puzzleString.slice(index[row][0], index[row][1])
+    return myRow.includes(value) ? false : true
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    let myColumn = ''
+    for (let index = column; index < puzzleString.length; index+=9) {
+      myColumn += puzzleString[index]
+    }
+    return myColumn.includes(value) ? false : true
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
